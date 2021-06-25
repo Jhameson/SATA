2203c37b1ad1cb429b9f3ad4c8962ce2ecd7fd63
fit-jhameson-farias <jhameson.farias@fitbank.com.br>
2021-06-25 16:14:57 -0300

    feature: inserindo bootstrap

---
+++ assets/js/search.js
@@ -0,0 +1,27 @@
+
+
+
+// const formInicial = document.querySelector('.form-inicial');
+
+
+// formInicial.addEventListener('submit', function (event) {
+//     event.preventDefault();
+
+//     const selectService = formInicial.querySelector('.select-service');
+//     const servico = selectService.options[selectService.selectedIndex];
+
+//     const cidade = formInicial.querySelector('.cidadeEstado');
+
+//     // const ver = false;
+//     // (cidade.value.length < 3) ? ver = true : console.log();
+   
+//     // (ver === true) ? alertar() : '';
+
+//     // function alertar(){
+//     //     alert('')
+//     // }
+
+//     console.log(`serviço ${servico.text} e cidade ${cidade.value}`);
+    
+// });
+
